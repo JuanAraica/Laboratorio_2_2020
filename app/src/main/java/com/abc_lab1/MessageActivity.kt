21eaea95ae288ccf223751d9e1fbeda7a79364c7
aray <juanaraica149@gmail.com>
2020-05-04 21:06:34 -0600

    Repositorio corregido

--- app/src/main/java/com/abc_lab1/MessageActivity.kt
+++ app/src/main/java/com/abc_lab1/MessageActivity.kt
@@ -5,7 +5,10 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MessageActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
         val intent = intent
